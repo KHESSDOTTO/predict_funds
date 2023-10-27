b7719341e88d595fe21c74443070b4705a7c6d65
Khess Santomauro Dotto <khessdotto@gmail.com>
2023-10-27 09:08:30 -0300

    Changed name of IndigoButtonHover to IndigoButton.

--- components/indigoButton.tsx
+++ components/indigoButton.tsx
@@ -1,4 +1,4 @@
-function IndigoButtonHover(props) {
+function IndigoButton(props) {
   const style =
     "rounded-md bg-gradient-to-b from-indigo-600 to-indigo-400 text-white font-semibold py-2 px-8 mt-8 border-2 border-indigo-800 hover:text-xl hover:transition-all hover:text-yellow-500 hover:underline";
 
@@ -9,4 +9,4 @@ function IndigoButtonHover(props) {
   );
 }
 
-export default IndigoButtonHover;
+export default IndigoButton;
