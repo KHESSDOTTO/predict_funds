92a163cefcf90ecd2530b3fb98e71e99b9d327ea
Khess Santomauro Dotto <khessdotto@gmail.com>
2023-12-27 10:07:32 -0300

    Excluded buttons optimistic/pessimistic/netFunding from chartSection. Added title to the chartsand second chart var. cota. CHanged color of the fill on both area charts.

--- components/sections/netFunding/dashboard/chartSection.tsx
+++ components/sections/netFunding/dashboard/chartSection.tsx
@@ -47,7 +47,7 @@ export default function ChartSection({ data }: ChartSectionProps) {
 
   return (
     <div className="col-start-6 col-span-12">
-      <div className="flex justify-around items-end">
+      {/* <div className="flex justify-around items-end">
         <div className="w-1/3 md:pl-2" onClick={togglePessimistic}>
           <OptionButtonRed>Forecast - 1 std</OptionButtonRed>
         </div>
@@ -57,14 +57,25 @@ export default function ChartSection({ data }: ChartSectionProps) {
         <div className="w-1/3 md:pr-2" onClick={toggleOptimistic}>
           <OptionButtonGreen>Forecast + 1 std</OptionButtonGreen>
         </div>
-      </div>
-      <div className="bg-gray-900 pt-4 lg:rounded-xl lg:pl-8 box-shadow shadow-md shadow-indigo-900 lg:shadow-sm">
+      </div> */}
+      <h1 className="my-4 font-semibold text-lg text-center border-b border-black mx-[32vw] lg:indent-2 lg:mx-4 lg:text-left">
+        Net Funding
+      </h1>
+      <div className="bg-gray-900 pt-4 lg:rounded-xl box-shadow shadow-md shadow-indigo-900 lg:shadow-sm lg:pl-8">
         <ResponsiveContainer height={300}>
           <AreaChart data={data}>
             <defs>
               <linearGradient id="customIndigo" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="10%" stopColor="#8884d8" stopOpacity={0.9} />
-                <stop offset="90%" stopColor="#8884d8" stopOpacity={0.2} />
+                <stop
+                  offset="40%"
+                  stopColor="rgb(180, 180, 255)"
+                  stopOpacity={0.85}
+                />
+                <stop
+                  offset="60%"
+                  stopColor="rgb(180, 180, 255)"
+                  stopOpacity={0.75}
+                />
               </linearGradient>
               <linearGradient id="customRed" x1="0" y1="0" x2="0" y2="1">
                 <stop
@@ -98,7 +109,7 @@ export default function ChartSection({ data }: ChartSectionProps) {
               fontSize={12}
               tickLine={false}
               tickFormatter={(DT_COMPTC) => {
-                return format(DT_COMPTC, `d, MMM/yy`);
+                return format(DT_COMPTC, `dd/MM/yy`);
               }}
             />
             <YAxis
@@ -112,7 +123,7 @@ export default function ChartSection({ data }: ChartSectionProps) {
               <Area
                 type="monotone"
                 dataKey="CAPTC_LIQ"
-                stroke="rgb(70, 0, 100)"
+                stroke="rgb(100, 0, 120)"
                 fill="url(#customIndigo)"
               ></Area>
             )}
@@ -134,7 +145,80 @@ export default function ChartSection({ data }: ChartSectionProps) {
               )} */}
             <Tooltip
               content={
-                <CustomTooltip
+                <CustomTooltipIndigo
+                  showNetFunding={showNetFunding}
+                  // showOptimistic={showOptimistic}
+                  // showPessimistic={showPessimistic}
+                />
+              }
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+      <h1 className="my-4 font-semibold text-lg text-center border-b border-black mx-[32vw] lg:indent-2 lg:mx-4 lg:text-left">
+        Variação - Cota
+      </h1>
+      <div className="bg-gray-900 pt-4 lg:rounded-xl box-shadow shadow-md shadow-indigo-900 lg:shadow-sm lg:pl-8">
+        <ResponsiveContainer height={300}>
+          <AreaChart data={data}>
+            <defs>
+              <linearGradient id="customYellow" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="10%"
+                  stopColor="rgb(200, 200, 100)"
+                  stopOpacity={0.9}
+                />
+                <stop
+                  offset="90%"
+                  stopColor="rgb(200, 200, 100)"
+                  stopOpacity={0.7}
+                />
+              </linearGradient>
+            </defs>
+            <XAxis
+              dataKey="DT_COMPTC"
+              tick={{ fill: "rgb(230, 230, 230)" }}
+              height={30}
+              fontSize={12}
+              tickLine={false}
+              tickFormatter={(DT_COMPTC) => {
+                return format(DT_COMPTC, `dd/MM/yy`);
+              }}
+            />
+            <YAxis
+              tick={{ fill: "rgb(230, 230, 230)" }}
+              tickFormatter={(num) => `R$${String(num.toFixed(2))}`}
+              width={70}
+              fontSize={12}
+            />
+            <CartesianGrid vertical={false} stroke="rgb(170, 150, 255)" />
+            {showNetFunding && (
+              <Area
+                type="monotone"
+                dataKey="VL_QUOTA"
+                stroke="rgb(150, 150, 75)"
+                fill="url(#customYellow)"
+              ></Area>
+            )}
+            {/* {showPessimistic && (
+                <Area
+                  type="monotone"
+                  dataKey="std1Less"
+                  stroke="red"
+                  fill="url(#customRed)"
+                ></Area>
+              )} */}
+            {/* {showOptimistic && (
+                <Area
+                  type="monotone"
+                  dataKey="std1More"
+                  stroke="green"
+                  fill="url(#customGreen)"
+                ></Area>
+              )} */}
+            <Tooltip
+              content={
+                <CustomTooltipYellow
                   showNetFunding={showNetFunding}
                   // showOptimistic={showOptimistic}
                   // showPessimistic={showPessimistic}
@@ -148,7 +232,7 @@ export default function ChartSection({ data }: ChartSectionProps) {
   );
 }
 
-function CustomTooltip({
+function CustomTooltipIndigo({
   active,
   payload,
   label,
@@ -159,7 +243,7 @@ CustomTootipProps) {
   console.log(JSON.stringify(payload));
   if (active && label && payload) {
     return (
-      <div className="bg-black/30 text-white p-2 rounded-md shadow-indigo-700 shadow-md">
+      <div className="bg-black/80 text-white p-2 rounded-md shadow-indigo-700 shadow-sm">
         <h4 className="font-semibold">{format(label, "d, MMM, yy")}</h4>
         {/* {showPessimistic && (
           <p>Less 1std: R${payload[0].payload.std1Less.toFixed(2)}mln</p>
@@ -168,6 +252,37 @@ CustomTootipProps) {
           <p>
             Net Funding: R$
             {payload[0].payload.CAPTC_LIQ.toFixed(2).toLocaleString("en-US")}
+          </p>
+        )}
+        {/* {showOptimistic && (
+          <p>More 1std: R${payload[0].payload.std1More.toFixed(2)}mln</p>
+        )} */}
+      </div>
+    );
+  }
+  return <></>;
+}
+
+function CustomTooltipYellow({
+  active,
+  payload,
+  label,
+  showNetFunding,
+}: // showOptimistic,
+// showPessimistic,
+CustomTootipProps) {
+  console.log(JSON.stringify(payload));
+  if (active && label && payload) {
+    return (
+      <div className="bg-black/80 text-white p-2 rounded-md shadow-yellow-700 shadow-sm">
+        <h4 className="font-semibold">{format(label, "d, MMM, yy")}</h4>
+        {/* {showPessimistic && (
+          <p>Less 1std: R${payload[0].payload.std1Less.toFixed(2)}mln</p>
+        )} */}
+        {showNetFunding && (
+          <p>
+            Net Funding: R$
+            {payload[0].payload.VL_QUOTA.toFixed(2).toLocaleString("en-US")}
           </p>
         )}
         {/* {showOptimistic && (
