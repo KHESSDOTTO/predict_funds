74d29cea444254be12fb71adeb9c16eb02073090
Khess Santomauro Dotto <khessdotto@gmail.com>
2024-01-23 10:08:50 -0300

    Added minicharts to myCenarios on mobile.

--- pages/loggedin/my_cenarios/index.tsx
+++ pages/loggedin/my_cenarios/index.tsx
@@ -89,9 +89,7 @@ export default function MyCenarios() {
   return (
     <div className="min-h-screen relative">
       {user && <Header user={user} />}
-      <h1 className="text-center text-2xl font-semibold underline py-6">
-        My Cenarios
-      </h1>
+      <h1 className="text-center text-3xl font-semibold py-6">My Cenarios</h1>
       <section
         id="cenarios"
         className="mb-8 flex flex-col gap-6 lg:justify-center lg:items-center lg:gap-0"
@@ -100,7 +98,7 @@ export default function MyCenarios() {
           return (
             <div
               key={cE.id.toString()}
-              className="border-2 border-black rounded-lg bg-white/90 shadow-md shadow-gray-400 pt-2 mx-8 pb-4 px-4 flex flex-col items-center lg:items-start lg:w-[60%] lg:mb-6"
+              className="border-2 border-black rounded-lg bg-white/80 shadow-md shadow-gray-400 pt-2 mx-8 pb-4 px-4 flex flex-col items-center lg:items-start lg:w-[60%] lg:mb-6"
             >
               <h2 className="text-lg font-semibold my-2 indent-1 w-2/3 pl-1 border-b border-black lg:indent-2 lg:w-11/12">
                 Cenario {cI + 1}
@@ -133,7 +131,7 @@ export default function MyCenarios() {
                     </li>
                   </ul>
                 </div>
-                <div className="hidden lg:block lg:w-full">
+                <div className=" lg:block lg:w-full">
                   <ChartSection data={cE.data} wPredList={false} />
                 </div>
                 <button
