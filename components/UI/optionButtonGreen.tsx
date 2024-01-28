bf2479d3275627074957da14ba9a85134d7d5758
Khess Santomauro Dotto <khessdotto@gmail.com>
2024-01-28 11:46:01 -0300

    Fixed other type problems that affected deploy.

--- components/UI/optionButtonGreen.tsx
+++ components/UI/optionButtonGreen.tsx
@@ -1,6 +1,6 @@
-import { ButtonProps } from "@/utils/types";
+import { ButtonPropsType } from "@/utils/types";
 
-function OptionButtonGreen(props: ButtonProps) {
+function OptionButtonGreen(props: ButtonPropsType) {
   const btnClass =
     "rounded-sm flex justify-center flex-col items-center border-b box-shadow font-bold py-2 px-4 w-64 mt-2 w-full hover:text-green-900 transition-all hover:bg-gradient-to-t hover:from-green-200/60 hover:to-transparent";
   return <button className={btnClass}>{props.children}</button>;
--- components/UI/optionButtonRed.tsx
+++ components/UI/optionButtonRed.tsx
@@ -1,6 +1,6 @@
-import { ButtonProps } from "@/utils/types";
+import { ButtonPropsType } from "@/utils/types";
 
-function OptionButtonRed(props: ButtonProps) {
+function OptionButtonRed(props: ButtonPropsType) {
   const btnClass =
     "rounded-sm flex justify-center flex-col items-center border-b box-shadow font-bold py-2 px-4 w-64 mt-2 w-full hover:text-red-900 transition-all hover:bg-gradient-to-t hover:from-red-200/70 hover:to-transparent";
   return <button className={btnClass}>{props.children}</button>;
