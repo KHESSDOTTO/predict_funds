32765aa73cb76e8295fde980346c251f91765696
Khess Santomauro Dotto <khessdotto@gmail.com>
2024-01-28 11:44:10 -0300

    Fixed type problems that affected deploy.

--- components/UI/buttonRed.tsx
+++ components/UI/buttonRed.tsx
@@ -1,6 +1,6 @@
-import { ButtonProps } from "@/utils/types";
+import { ButtonPropsType } from "@/utils/types";
 
-function ButtonRed(props: ButtonProps) {
+function ButtonRed(props: ButtonPropsType) {
   const style =
     "font-serif py-1 px-4 border-2 border-red-900 rounded-md bg-gradient-to-b from-red-900 to-red-500 text-white transition-all hover:text-yellow-300/90 hover:underline";
   return <button className={style}>{props.children}</button>;
