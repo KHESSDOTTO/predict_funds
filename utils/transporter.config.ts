538b8d97505fcba9eb35784164d742ea527345b3
Khess Santomauro Dotto <khess@injoy.ag>
2024-12-08 08:56:07 -0300

    Fixed host name.

--- utils/transporter.config.ts
+++ utils/transporter.config.ts
@@ -1,15 +1,13 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.zoho.com',
+  host: 'smtppro.zoho.com',
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_ADDRESS,
     pass: process.env.EMAIL_PWD,
   },
-  logger: true, // Enable logging
-  debug: true, // Enable debug output
 });
 
 export default transporter;
