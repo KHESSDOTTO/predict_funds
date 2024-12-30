96db502f38c4c19cb04a652e67890bdebdf72c16
Khess Santomauro Dotto <khess@injoy.ag>
2024-12-30 10:35:27 -0300

    Finished export for correlCards on desktop. Yet to display export button opn mobile.

--- components/general/correlCardsSection/correlCardsSectionFunctions.ts
+++ components/general/correlCardsSection/correlCardsSectionFunctions.ts
@@ -11,13 +11,10 @@ function exportCorrels({
   const workbook = XLSX.utils.book_new();
   const currTime = new Date();
   const fileName = "export_" + currTime.toISOString() + ".xlsx";
-
-  // To do:
-  // create two sheets, each one with title of janela_em_meses_<periodo>
-  // and content of the corresponding correls.
   
   correls.forEach(currCorrelPeriod => {
     const periodElement = currCorrelPeriod.filter(currField => currField[0] === 'janela_em_meses');
+    const sheetData = currCorrelPeriod.filter(currField => currField[0] !== 'janela_em_meses');
 
     if (periodElement.length !== 1) {
       consoleLog({ periodElement });
@@ -25,7 +22,6 @@ function exportCorrels({
     }
 
     const sheetTitle = periodElement.toString().replaceAll(",", "_");
-    const sheetData = [[]];
 
     const sheet = XLSX.utils.aoa_to_sheet(sheetData);
     XLSX.utils.book_append_sheet(workbook, sheet, sheetTitle);
