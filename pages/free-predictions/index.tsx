62910c8987217bfbf5b4f65fadd723dd2155cc21
Khess Santomauro Dotto <khessdotto@gmail.com>
2025-05-05 10:40:11 -0300

    Checkpoint new css home logged out. landing main page.

---
+++ pages/free-predictions/index.tsx
@@ -0,0 +1,8 @@
+export default function FreePredictions()
+{
+    return (
+        <div>
+            <div>Here are my preds</div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,26 +6,35 @@ export default function Home() {
 
   return (
     <main className="bg-landing bg-cover z-0 relative">
-      <div className="z-0 hidden lg:block bg-gradient-to-l from-black via-black/10 absolute top-0 left-0 h-full w-[50%]"></div>
-      <div className="z-0 hidden lg:block bg-gradient-to-r from-black via-black/10 absolute top-0 left-[50%] h-full w-[50%]"></div>
       <div className="text-white flex min-h-screen flex-col items-center justify-around p-8 z-20 lg:p-16 animate-fadeIn-l-r lg:animate-fadeIn">
-        <h1 className="text-center z-10">
-          <LogoPredict bold={true} />
+        <h1 className="relative text-center z-10 p-8">
+          <div className="black-blur w-[1000px] h-[300px] hidden lg:block"></div>
+          <div className="relative z-10">
+            <LogoPredict bold={true} />
+          </div>
         </h1>
-        <div className="relative h-28 w-full text-xl lg:top-16 font-semibold">
+        <div className="relative overflow-visible h-48 w-full text-xl lg:top-16 flex flex-col gap-6 lg:flex-row justify-center items-center lg:w-[50%] font-semibold">
+          <div className="absolute gray-blur w-[150%] h-[400px] hidden lg:block"></div>
           <Link
             href={"/signup"}
-            className="py-1 absolute top-0 right-[50%] translate-x-[50%] hover:text-yellow-600 md:hover:scale-110 w-28 text-center transition-all duration-200"
+            className="z-10 py-1 hover:text-yellow-600 md:hover:scale-110 lg:w-1/3 text-center transition-all duration-200"
             style={smTextShadow}
           >
             Sign up
           </Link>
           <Link
             href={"/login"}
-            className="py-1 absolute bottom-0 right-[50%] translate-x-[50%] hover:text-yellow-600 md:hover:scale-110 w-28 text-center transition-all duration-200"
+            className="z-10 py-1 hover:text-yellow-600 hover:scale-110 lg:w-1/3 text-center transition-all duration-200"
             style={smTextShadow}
           >
             Login
+          </Link>
+          <Link
+            href={"/free-predictions"}
+            className="z-10 py-1 hover:text-yellow-600 md:hover:scale-110 lg:w-1/3 text-center transition-all duration-200"
+            style={smTextShadow}
+          >
+            Predictions
           </Link>
         </div>
       </div>
