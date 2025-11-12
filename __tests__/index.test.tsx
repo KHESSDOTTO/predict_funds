76e8df52daeb32154f789c065d6ce556ebbb5e23
Khess Santomauro Dotto <khessdotto@gmail.com>
2025-11-12 07:49:20 -0300

    Vitest working for non DOM tests.

--- __tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import Home from "@/pages";
-
-test("Home", () => {
-  render(<Home />);
-  const element = screen.getByText("Sign up");
-  expect(element).toBeTruthy();
-});
